c0add3ed3c44e91b43be923eae1c751ccdf8bfe3
Marcin Majewski <martinpl@gmail.com>
2022-06-11 16:49:14 +0200

    upgrades to room admin (search, filter, fieldsets, list_display

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,7 +7,58 @@ from .models import Facility, Room, RoomType, Amenity, HouseRule, Photo
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Time", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Space", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Host", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        # "description",
+        "country",
+        "city",
+        "price",
+        "address",
+        "beds",
+        "bedrooms",
+        "baths",
+        "guests",
+        "instant_book",
+        "host",
+        "room_type",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",  # use __ instead of .
+        "host__gender",
+        "host",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    search_fields = ("city", "name", "description", "host__username")
+    # "^city"
+    # /ref/contrib/admin/#django.contrib.admin.ModelAdmin.search_fields
+    # "host__username" __ instead of . because it's a foreign key
 
 
 @admin.register(RoomType, Facility, Amenity, HouseRule)
