8c7d5471665fe53b3ecb1be0027f1478781e85e2
Marcin Majewski <martinpl@gmail.com>
2022-06-28 21:37:41 +0200

    manual pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,18 +1,26 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
+import math
 from django.shortcuts import render
 from . import models
+
 
 # Create your views here.
 
 
 def list_all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page_size = 10
+    page = int(request.GET.get("page", 1) or 1)
+    all_rooms = models.Room.objects.all()[(page - 1) * page_size : page * page_size]
+    page_count = math.ceil(models.Room.objects.count() // page_size) + 1
 
     return render(
         request,
         "rooms/list_all_rooms.html",
         context={
             "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
         },
     )
