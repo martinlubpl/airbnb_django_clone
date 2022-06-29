f445671361515c2eaa4097848eb55a67cfa166d7
Marcin Majewski <martinpl@gmail.com>
2022-06-29 13:07:15 +0200

    catch exception when page too high

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,9 @@
 # from django.shortcuts import render
 # from django.http import HttpResponse
 # import math
-from django.shortcuts import render
-from django.core.paginator import Paginator
+
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 
@@ -12,9 +13,6 @@ from . import models
 def list_all_rooms(request):
 
     page = int(request.GET.get("page", 1) or 1)
-    # page_size = 10
-    # all_rooms = models.Room.objects.all()[(page - 1) * page_size : page * page_size]
-    # page_count = math.ceil(models.Room.objects.count() // page_size) + 1
     room_list = models.Room.objects.all()  # lazy
     paginator = Paginator(
         room_list,
@@ -22,15 +20,17 @@ def list_all_rooms(request):
         orphans=5,
     )  # attach 5 or less orphans to previous page
     # rooms = paginator.get_page(page)
-    rooms = paginator.page(int(page))
-    print(dir(rooms))
-    return render(
-        request,
-        "rooms/list_all_rooms.html",
-        context={
-            "page": rooms,
-            #     "page": page,
-            #     "page_count": page_count,
-            #     "page_range": range(1, page_count + 1),
-        },
-    )
+
+    # with page() you can catch exceptions
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/list_all_rooms.html",
+            context={
+                "page": rooms,
+            },
+        )
+    except EmptyPage:
+        # rooms = paginator.page(1)
+        return redirect("/")
