6b2b6b3cdcc7e7a30af97b0e93970cb3632111dc
Marcin Majewski <martinpl@gmail.com>
2022-07-02 12:26:35 +0200

    add price, guests, bedrooms, baths, beds, amenities, facilities to search form

--- rooms/views.py
+++ rooms/views.py
@@ -51,16 +51,34 @@ def search(request):
     room_type = int(request.GET.get("room_type", 0))
     # print(city)
     room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    # house_rules = models.HouseRule.objects.all()
+
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
 
     chosen = {
         "selected_city": city,
         "selected_country": country,  # chosen
         "selected_room_type": room_type,  # chosen
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
     }
 
     choices = {
         "countries": countries,  # all
         "room_types": room_types,  # all
+        "amenities": amenities,
+        "facilities": facilities,
     }
 
     return render(
