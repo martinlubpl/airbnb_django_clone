80f969f85c347526597cc30fc022da07be4f1187
Marcin Majewski <martinpl@gmail.com>
2022-07-03 20:07:12 +0200

    clean password: check if password is correct

--- users/forms.py
+++ users/forms.py
@@ -15,9 +15,22 @@ class LoginForm(forms.Form):
         email = self.cleaned_data.get("email")
         try:
             # ! not email=email since username is email
-            user = User.objects.get(username=email)
+            User.objects.get(username=email)
             return email
         except User.DoesNotExist:
             raise forms.ValidationError("User does not exist")
+
+    def clean_password(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = User.objects.get(username=email)
+            # check_password => True if the given raw string is the correct
+            if user.check_password(password):
+                return password
+            else:
+                raise forms.ValidationError("Password is incorrect")
+        except User.DoesNotExist:
+            pass
 
     # def clean(self): can also be used
