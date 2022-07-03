2a4a946b8c7f542a5b154ad5803f1ab5e4e64b12
Marcin Majewski <martinpl@gmail.com>
2022-07-03 19:51:56 +0200

    validate if user with email exists

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import User
 
 
 class LoginForm(forms.Form):
@@ -8,3 +9,15 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput
     )  # cannot use PasswordInput directly
+
+    # clean_<fieldname>
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            # ! not email=email since username is email
+            user = User.objects.get(username=email)
+            return email
+        except User.DoesNotExist:
+            raise forms.ValidationError("User does not exist")
+
+    # def clean(self): can also be used
